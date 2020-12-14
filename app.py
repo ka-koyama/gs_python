c89e303d9d980c17ae975048a83b2c619d28d476
kazuya kouyama <k.kouyama0212@gmail.com>
2020-12-14 22:55:38 +0900

    gs expansion_python
    chatbot-news

---
+++ app.py
@@ -0,0 +1,56 @@
+import json
+from urllib.request import urlopen
+from random import shuffle
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """初期画面を表示します."""
+    return render_template("index.html")
+
+@app.route("/api/recommend_article")
+def api_recommend_article():
+    """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
+    
+    #**** ここを実装します（基礎課題） ****
+
+    #はてブのホットエントリーページのHTMLを取得
+    with urlopen("http://feeds.feedburner.com/hatena/b/hotentry") as res:
+        html = res.read().decode("utf-8")
+
+    #BeautifulSoupでHTMLを読み込む
+    soup = BeautifulSoup(html, "html.parser")
+
+    #記事一覧を取得
+    items = soup.select("item")
+
+    #ランダムに1件取得
+    shuffle(items)
+    item = items[0]
+    #print(item)→書かなくても大丈夫そう
+    
+    #以下の形式で返却
+    return json.dumps({
+        "content" : item.find("title").string,
+        # "link" : item.find("link").string
+        "link": item.get('rdf:about')   #'rdf:about'とは？
+    })
+    
+
+  
+
+@app.route("/api/xxxx")
+def api_xxxx():
+    """
+        **** ここを実装します（発展課題） ****
+        ・自分の好きなサイトをWebスクレイピングして情報をフロントに返却します
+        ・お天気APIなども良いかも
+        ・関数名は適宜変更してください
+    """
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5004)
